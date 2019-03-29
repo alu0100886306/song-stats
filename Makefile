3024be386302fb5abc4d5d8452f6cce7f2dd74e1
CHRISTIAN <alu0100886306@cc1231.etsii.ull.es>
2019-03-29 12:46:49 +0000

    score añadido

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
 run:
 	python3 add-song-stats.py ${ARGS}
 	
-install:
-	pip3 install -r tqmd
--- add-song-stats.py
+++ add-song-stats.py
@@ -1,18 +1,19 @@
 import argparse
 import csv
 import os
-from tqdm import tqdm
 
 HEADER = ['Track Name', 'Artist', 'Value', 'TopPos', 'TopMonth', 'Year','danceability',
 	  'energy', 'key', 'loudness', 'mode', 'speechiness',
     	  'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
-    	  'duration_ms', 'time_signature']
+    	  'duration_ms', 'time_signature', 'score']
 songs_array = []
 
 def song_stats_from_csv(input_filepath):
 	region = os.path.splitext(os.path.basename(input_filepath))[0]
 
 	output_filepath = 'song_stats_' + region + '.csv'
+
+	max_value = {'2017': 0, '2018': 0}
 
 	with open(input_filepath, 'r') as csv_input:
 	
@@ -22,12 +23,14 @@ def song_stats_from_csv(input_filepath):
 		# Count the num of lines the csv file has (used for the progress bar)
 		lines = [line for line in csv_input]
 
-		for row in tqdm(csv.reader(lines), total=len(lines)):
+		for row in csv.reader(lines):
 			found = False
 			if len(songs_array)>0:
 				for song in songs_array:
 					if song['TN'] == row[1] and song['A'] == row[2] and song['Y'] == row[5]:
 						song['V'] += 101-int(row[0])
+						if max_value[song['Y']] < song['V']:
+							max_value[song['Y']] = song['V']
 						if row[0] < song['TP']:
 							song['TP'] = row[0]
 							song['M'] = row[6]
@@ -36,10 +39,16 @@ def song_stats_from_csv(input_filepath):
 			if not found:
 				aux = {'TN':row[1], 'A':row[2], 'V':(101-int(row[0])),
 					'TP':row[0], 'M':row[6], 'Y':row[5]}
+				
+				if max_value[aux['Y']] < aux['V']:
+					max_value[aux['Y']] = aux['V']
 				#adding the rest of attr
 				for i in range(9,22):
 					aux['a' + str(i-8)] = row[i]
 				songs_array.append(aux)
+
+	for song in songs_array:
+		song['S'] = int(round(song['V']*100/max_value[song['Y']] , 0))
 
 	with open(output_filepath, 'w') as csv_output:
 
@@ -49,8 +58,8 @@ def song_stats_from_csv(input_filepath):
 			writer.writerow(song.values())
 		
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Get songs stats from waf.csv')
-	parser.add_argument('input_file', help='input file in waf.csv format')
+	parser = argparse.ArgumentParser(description='Add audio features to .csv')
+	parser.add_argument('input_file', help='input file in .csv format')
 	args = parser.parse_args()
 
 	song_stats_from_csv(args.input_file)
